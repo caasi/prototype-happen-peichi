323bb6e465109464db6ba747e114d25324b79ac2
caasi Huang <caasi.igd@gmail.com>
2014-04-16 18:24:10 +0800

    as a gift?

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 (function(){
-
+  $.getJSON("data.json", function(data){
+    var counter, update;
+    counter = 0;
+    $('#countdown .clock').countdown(data.date, function(it){
+      return $(this).html(it.strftime("%D days %H:%M:%S"));
+    });
+    return (update = function(){
+      var d;
+      d = data.messages[counter];
+      $('#container').css('background-image', "url('" + d.image + "')");
+      $('#message').html(d.message);
+      counter = (counter + 1) % data.messages.length;
+      return setTimeout(update, data.interval * 1000);
+    })();
+  });
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,1 +1,10 @@
-# foobar
+data <- $.getJSON "data.json"
+counter = 0
+$('#countdown .clock').countdown data.date, ->
+  $(this).html it.strftime "%D days %H:%M:%S"
+do update = ->
+  d = data.messages[counter]
+  $('#container').css \background-image "url('#{d.image}')"
+  $('#message').html d.message
+  counter := (counter+1) % data.messages.length
+  setTimeout update, data.interval * 1000
